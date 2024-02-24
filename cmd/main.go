a10be4b04d9c80120b466a4e257bc79eae3499b4
RolandWarburton <warburtonroland@gmail.com>
2024-02-24 23:36:29 +1100

    Remove printNextDepartures

--- cmd/main.go
+++ cmd/main.go
@@ -55,37 +55,6 @@ func printFormatted[Type any](data []Type, format string, delimiter string) {
 
 }
 
-func printNextDepartures(totalDepartures int) {
-	// get the departures for a stop on a route
-	departures, err := app.GetDepartures(1016, 2, "?expand=All&include_geopath=true")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// get the next N departures in a certain direction
-	nextBWDepartures, err := app.GetNextDepartureTowards(departures, 1, totalDepartures)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	nextDepartures := []string{}
-	for i := 0; i < len(nextBWDepartures); i++ {
-		if err == nil {
-			layout := "2006-01-02T15:04:05Z"
-			departureTime, err := time.Parse(layout, nextBWDepartures[i].ScheduledDepartureUTC)
-			if err != nil {
-				nextDepartures = append(nextDepartures, "ERROR")
-			} else {
-				formattedTime := departureTime.Format("2-1-2006 3:4 PM")
-				nextDepartures = append(nextDepartures, formattedTime)
-			}
-		}
-	}
-	fmt.Println(nextDepartures)
-}
-
 func routeAction(cCtx *cli.Context, format string, delimiter string) error {
 	// get routes
 	routes, _ := app.GetRoutes(cCtx.Args().First())
